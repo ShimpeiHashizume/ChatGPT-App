d08a46354e94a7516c309d8d02618fe64fca1c05
hashizume <hashizume@fabric-arts.co.jp>
2024-01-04 13:31:17 +0900

    [update]ひな型作成

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -1,22 +1,27 @@
-import React from 'react'
+import React from "react";
+import { BiLogOut } from "react-icons/bi";
 
 const Sidebar = () => {
   return (
-    <div className='bg-custom-blue h-full overflow-y-auto px-5 flex-col'>
-      <div className='flex-grow'>
+    <div className="bg-custom-blue h-full overflow-y-auto px-5 flex flex-col">
+      <div className="flex-grow">
         <div className="flex justify-evenly items-center border mt-2 rounded-md cursor-pointer hover:bg-blue-800 duration-150">
-          <span className='text-white p-4 text-2xl'>＋</span>
-          <h1 className='text-white text-xl font-semibold p-4'>New Chat</h1>
+          <span className="text-white p-4 text-2xl">＋</span>
+          <h1 className="text-white text-xl font-semibold p-4">New Chat</h1>
         </div>
         <ul>
-          <li className='cursor-pointer border-b p-4 text-slate-100 hover:bg-slate-700 duration-150'>Room1</li>
-          <li className='cursor-pointer border-b p-4 text-slate-100 hover:bg-slate-700 duration-150'>Room2</li>
-          <li className='cursor-pointer border-b p-4 text-slate-100 hover:bg-slate-700 duration-150'>Room3</li>
-          <li className='cursor-pointer border-b p-4 text-slate-100 hover:bg-slate-700 duration-150'>Room4</li>
+          <li className="cursor-pointer border-b p-4 text-slate-100 hover:bg-slate-700 duration-150">Room1</li>
+          <li className="cursor-pointer border-b p-4 text-slate-100 hover:bg-slate-700 duration-150">Room2</li>
+          <li className="cursor-pointer border-b p-4 text-slate-100 hover:bg-slate-700 duration-150">Room3</li>
+          <li className="cursor-pointer border-b p-4 text-slate-100 hover:bg-slate-700 duration-150">Room4</li>
         </ul>
       </div>
+      <div className="text-lg flex items-center justify-evenly mb-2 cursor-pointer p-4 text-slate-100 hover:bg-slate-700 duration-150">
+        <BiLogOut />
+        <span>ログアウト</span>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
